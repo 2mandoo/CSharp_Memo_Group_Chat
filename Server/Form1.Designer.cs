96fe5c183acca759a209240667b902d3a2c8dcd2
2mandoo <110588102+2mandoo@users.noreply.github.com>
2024-12-18 13:30:52 +0900

    server commit

---
+++ Server/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ChatServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.btnStart.Font = new System.Drawing.Font("굴림", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnStart.Location = new System.Drawing.Point(48, 192);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(111, 27);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Server Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtServer
+            // 
+            this.txtServer.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtServer.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtServer.Location = new System.Drawing.Point(31, 24);
+            this.txtServer.Multiline = true;
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(149, 162);
+            this.txtServer.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(212, 231);
+            this.Controls.Add(this.txtServer);
+            this.Controls.Add(this.btnStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ChatServer";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtServer;
+    }
+}
+
---
+++ Server/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net; // 추가
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace ChatServer
+{
+    public partial class Form1 : Form
+    {
+        private TcpListener server;
+        private List<TcpClient> clients = new List<TcpClient>();
+        private Dictionary<TcpClient, string> clientNames = new Dictionary<TcpClient, string>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            server = new TcpListener(IPAddress.Any, 9000);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            server.Start();
+            txtServer.AppendText("Server started...\r\n");
+            AcceptClientsAsync();
+        }
+
+        private async void AcceptClientsAsync()
+        {
+            while (true)
+            {
+                TcpClient client = await server.AcceptTcpClientAsync();
+                clients.Add(client);
+                txtServer.AppendText("New client connected...\r\n");
+
+                // 첫 메시지를 통해 닉네임을 받음
+                ReceiveNickname(client);
+
+                //HandleClientAsync(client);
+            }
+        }
+
+        private async void ReceiveNickname(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+
+            int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+            string nickname = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+            // 클라이언트의 닉네임 저장 및 서버에 출력
+            clientNames[client] = nickname;
+            txtServer.Invoke((Action)(() =>
+            {
+                //txtServer.AppendText($"{nickname} 접속중\r\n");
+                DisplayClientList();
+            }));
+
+            HandleClientAsync(client);
+        }
+
+
+        private async void HandleClientAsync(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+
+            try
+            {
+                while (true)
+                {
+                    int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length); // 클라이언트 나갔을 때 예외 처리 필요
+                    if (bytesRead == 0) break;  // 연결 종료 시
+
+                    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    //txtServer.AppendText($"{message}\r\n"); // 서버엔 출력할 필요 없음
+                    BroadcastMessage(message, client);
+
+                } 
+            }
+            catch (IOException e)
+            {
+                Invoke((Action)(() =>
+                {
+                    txtServer.AppendText($"Network error: {e.Message}\r\n");
+                }));
+            }
+            finally
+            {
+                clients.Remove(client);
+                clientNames.Remove(client);
+                client.Close();
+                DisplayClientList();  // 클라이언트가 나가면 리스트 업데이트
+            }
+
+        }
+
+        private void SendClientList()
+        {
+            // 접속 중인 클라이언트　목록
+            string clientListMessage = "[접속중] " + string.Join(" | ", clientNames.Values);
+
+            // 모든 클라이언트에게 전송
+            byte[] data = Encoding.UTF8.GetBytes(clientListMessage);
+            foreach (var client in clients)
+            {
+                client.GetStream().Write(data, 0, data.Length);
+            }
+        }
+
+        private void BroadcastMessage(string message, TcpClient sender)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+
+            foreach (var client in clients)
+            {
+                //if (client != sender)
+                //{
+                    client.GetStream().Write(data, 0, data.Length);
+                //}
+            }
+        }
+        private void DisplayClientList()
+        {
+            txtServer.Clear();
+            //txtServer.AppendText("Connected clients:\r\n");
+            foreach (var name in clientNames.Values)
+            {
+                txtServer.AppendText($"[{name}] 접속중 (^-^)\r\n\r\n");
+            }
+
+            // 클라이언트들에게 목록 전송
+            SendClientList();
+        }
+
+    }
+}
